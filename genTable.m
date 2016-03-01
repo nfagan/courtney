e492fef24565376fa2b386a3dff819ff518e2b3b
nfagan <nick@fagan.org>
2016-03-01 17:10:03 -0500

    added help section

--- genTable.m
+++ genTable.m
@@ -1,3 +1,6 @@
+% possible outputs include 'n images', 'proportion', 'raw counts', or
+% 'average duration'. Define the region of interest as 'whole face' or 'roi'
+
 function M = genTable(saveData,region,outputType)
 
 region = lower(region); outputType = lower(outputType); %make lowercase
@@ -20,6 +23,8 @@ for i = 1:rows;
         ex = saveData{i,j}{1,rg};
         
         switch outputType;
+            case 'n images'
+                ex2 = length(ex.allDurations);
             case 'proportion'
                 ex2 = ex.proportion;
             case 'raw counts'
